e89d76d5844ab564256c872b5cfa22a26afdd97a
ntavasso <42741675+ntavasso@users.noreply.github.com>
2019-10-10 16:27:03 -0400

    did stuff

--- n320Coursework/labs/week6/js/app.js
+++ n320Coursework/labs/week6/js/app.js
@@ -4,21 +4,21 @@ var app = new Vue({
         gameOver: false,
         playerTurn: 1,
         grid: [
-            [ 0,0,0,0,0,0,0 ],
-            [ 0,0,0,0,0,0,0 ],
-            [ 0,0,0,0,0,0,0 ],
-            [ 0,0,0,0,0,0,0 ],
-            [ 0,0,0,0,0,0,0 ],
-            [ 0,0,0,0,0,0,0 ]
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0]
         ]
     },
     methods: {
-        selectCell: function(row, col) {
+        selectCell: function (row, col) {
 
             //get the row to place the puck at
             var moveRow = this.lowestMove(col);
-            
-            if(moveRow >= 0) {
+
+            if (moveRow >= 0) {
                 //copy grid to a temporary var
                 var tempGrid = this.grid.slice(0);
 
@@ -34,20 +34,71 @@ var app = new Vue({
                 //check for win
                 this.checkWin();
             }
-            
-        },
-        checkWin: function() {
-            //loop through all columns to check
 
-            //if win found, set over to true
         },
-        lowestMove: function(col) {
+        checkWin: function () {
+            var currentPuck = 0;
+            var previousPuck = 0;
+            var stack = 0;
+
+
+            //for loop for horizontal row
+            for (var row = 0; row <= 5; row++) {
+                for (var col = 0; col <= 6; col++) {
+
+                    //check for four in a row
+                    currentPuck = this.grid[row][col];
+                    //if the current puck matches the previous one, start a stack(horizontal)
+                    if (currentPuck == previousPuck && currentPuck != 0) {
+                        stack++;
+                    } else {
+                        stack = 0;
+                    }
+                    //if the current puck matches the previous one 3 times then the player wins
+                    if (stack == 3) {
+
+                        //game over occurs
+                        this.gameOver = true;
+                        this.player = currentPuck;
+                    }
+                    //set previous and current equal to each other, triggering the game over
+                    previousPuck = currentPuck;
+                }
+            }
+
+            //for loop for vertical row
+            for (var col = 0; col <= 6; col++) {
+                for (var row = 0; row <= 5; row++) {
+
+                    //check for four in a row
+                    currentPuck = this.grid[row][col];
+                    //if the current puck matches the previous one, start a stack(vertical)
+                    if (this.currentPuck = previousPuck && currentPuck != 0) {
+                        stack++;
+                    } else {
+                        stack = 0;
+                    }
+                    //if the current puck matches the previous one 3 times then the player wins
+                    if (stack == 3) {
+
+                        //game over occurs
+                        this.gameOver = true;
+                        this.player = currentPuck;
+
+                    }
+                    //set previous and current equal to each other, triggering the game over
+                    previousPuck = currentPuck;
+                }
+
+            }
+        },
+        lowestMove: function (col) {
             //start at the bottom of a col, loop upwards
-            for(var row = 5; row >= 0; row--) {
+            for (var row = 5; row >= 0; row--) {
                 //check to see if current row is free
-                if(this.grid[row][col]==0) {
+                if (this.grid[row][col] == 0) {
                     //if it is free, return the row index
-                    return(row);
+                    return (row);
                 }
             }
         }
