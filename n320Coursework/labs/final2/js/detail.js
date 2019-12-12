5325848dbd9b112a16b60f844a5c3183885c053d
ntavasso <42741675+ntavasso@users.noreply.github.com>
2019-12-12 18:06:28 -0500

    stuff

--- n320Coursework/labs/final2/js/detail.js
+++
@@ -1,8 +0,0 @@
-// $(document).ready(function () {
-//     var path = location.href;
-//     var url = new URL(path);
-//     id = url.searchParams.get("id");
-//     $("#content").css("display", "none");
-//     $("#"+ id).css("display", "block");
-//     console.log(id);
-// });
